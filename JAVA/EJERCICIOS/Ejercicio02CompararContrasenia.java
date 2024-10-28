eaed0eb1604b0f0841b706a0fcaba59be94bad2a
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-28 07:42:47 -0500

    Ejercicio Nº2: Comparar Contraseñas

---
+++ JAVA/EJERCICIOS/Ejercicio02CompararContrasenia.java
@@ -0,0 +1,33 @@
+package JAVA.EJERCICIOS;
+
+/*
+EJERCICIO DE PROGRAMACIÓN:
+Escribir un programa que almacene la cadena 
+de caracteres contraseña en una variable, 
+pregunte al usuario por la contraseña e 
+imprima por pantalla si la contraseña 
+introducida por el usuario coincide con 
+la guardada en la variable sin tener en 
+cuenta mayúsculas y minúsculas.
+ */
+import java.util.Scanner;
+
+public class Ejercicio02CompararContrasenia {
+    static Scanner entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        /* variables */
+        final String contraseniaGuardada = "admin123";
+        String contraseniaIngresada;
+        /* pedimos la contraseña al usuario */
+        System.out.println("Ingrese la contraseña por favor: ");
+        contraseniaIngresada = entrada.nextLine();
+        /* comparamos */
+        if (contraseniaGuardada.equalsIgnoreCase(contraseniaIngresada)) {
+            System.out.println("La contraseña ingresada coincide con al contraseña guardada");
+        } else {
+            System.out.println("La contraseña ingresada no coincide con al contraseña guardada");
+        }
+    }
+
+}
---
+++ PYTHON/EJERCICIOS/Ejercicio02CompararContrasenia.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+#variables
+contrasenia_guardada = "Admin123"
+contrasenia_introducida = input("Ingrese la contraseña: ")
+
+#comparamos las contraseñas 
+if contrasenia_guardada.casefold() == contrasenia_introducida.casefold():
+    print("La contraseña coincide con contraseña la guardada....")
+else:
+    print("La contraseña no coincide con contraseña la guardada....")
+    
+        
+    
