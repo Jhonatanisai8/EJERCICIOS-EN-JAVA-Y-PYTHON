d2f48f1e3c7d6e40e1eec61e68301d0bfc4a9743
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-18 07:53:55 -0500

    tipos de datos ne java

---
+++ JAVA/EJERCICIOS/CONDICIONALES/Ejercicio21TiposDatos.java
@@ -0,0 +1,22 @@
+package JAVA.EJERCICIOS.CONDICIONALES;
+
+public class Ejercicio21TiposDatos {
+    public static void main(String[] args) {
+        System.out.println("TIPOS DE DATOS EN JAVA");
+        // PRIMITIVOS 
+        byte numero = 122;
+        short valor = 32000;
+        int edad = 1200000000;
+        double precio = 12.42;
+        float altura = 56L;
+        long dato = Long.MAX_VALUE;
+        boolean mayorEdad = false;
+        char inicial = 'A';
+
+        // NO PRIMITIVOS 
+        String nombre = "Memo";
+        // Arreglos
+        int [] arreglo = {34,6,2,12,};
+        
+    }
+}
